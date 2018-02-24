57a1d559665792049034b193f9e87d14310b9cc0
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-02-25 01:17:28 +0600

    Update email1.php

--- email1.php
+++ email1.php
@@ -1,8 +1,12 @@
 <?php
 
-$email = $_POST['email'];
-
-echo $email;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_POST['email'];
+    if (empty($email)) {
+        echo "mail is empty";
+    } else {
+        echo $email;
 
 
 ?>
